30489d93a66559a7b9e40cbb176312cbb3279adf
VanshChauhan15 <97740365+VanshChauhan15@users.noreply.github.com>
2023-08-15 14:16:33 +0530

    first commit

---
+++ components/shared/Bottombar.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { sidebarLinks } from "@/constants";
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { useRouter,usePathname } from 'next/navigation';
+
+
+function Bottombar(){
+
+    const router = useRouter();
+    const pathname = usePathname();
+    return(
+        <section className="bottombar">
+            <div className="bottombar_container">
+            {sidebarLinks.map((link)=>{
+
+            const isActive = (pathname.includes(link.route) && link.route.length>1) || pathname===link.route
+
+            return(
+                <Link 
+                    href={link.route}
+                    key={link.label}
+                    className={`bottombar_link ${isActive && 'bg-primary-500'}`}
+                >
+                    <Image
+                        src={link.imgURL}
+                        alt={link.label}
+                        height={24}
+                        width={24}
+                    />
+                    <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                        {link.label.split(/\s+/)[0]}
+                    </p>
+                </Link>
+                )
+            })}
+            </div>
+        </section>
+    )
+}
+
+export default Bottombar;
---
+++ lib/mongoose.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery',true);
+
+    if(!process.env.MONGODB_URL) return console.log("MONGODB URL not found");
+
+    if(isConnected) return console.log("Already connected to mongodb");
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        isConnected=true;
+        console.log("Connected to mongodb");
+    } catch (error) {
+        console.log(error);
+    }
+
+}
